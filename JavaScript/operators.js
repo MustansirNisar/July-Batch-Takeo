fe3ff907e289d4a36d5737038921d5443e59f984
Mustansir Nisar <140247214+MustansirNisar@users.noreply.github.com>
2025-11-13 09:05:10 +0530

    Add files via upload

---
+++ JavaScript/operators.js
@@ -0,0 +1,28 @@
+let a = 10;
+let b = 3;
+
+// console.log(a + b)
+// console.log(a - b)
+// console.log(a * b)
+// console.log(a / b) //Quotient
+// console.log(a % b) //Remainder
+// console.log(a ** b)
+
+// let x = 10;
+// console.log(x++) prints 10, then x becomes 11
+// console.log(x)
+// console.log(x--)
+// console.log(x)
+// console.log(++x) x becomes 11, then prints 11
+// console.log(--x)
+
+// let x = 5;
+// console.log(x++); 
+// console.log(--x);
+// console.log(x)
+
+
+
+
+
+
