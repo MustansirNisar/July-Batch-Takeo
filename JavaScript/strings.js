59962704c265734ddf34a2ed61e699925458ef31
Mustansir Nisar <140247214+MustansirNisar@users.noreply.github.com>
2025-11-13 09:04:21 +0530

    Add files via upload

---
+++ JavaScript/strings.js
@@ -0,0 +1,33 @@
+// let fName = "   Ravin  "
+// let greet = "welcome to this session"
+
+// console.log(greet.length)
+// console.log(fName[3])
+// console.log(fName[0])
+// console.log(fName[2])
+
+// console.log(greet.toUpperCase())
+// console.log(greet.toLowerCase())
+
+// console.log(fName)
+// let newstring = fName.trim()
+// console.log(fName)
+// console.log(newstring)
+// let fName = "Ravin"
+// let greet = "welcome to this session"
+// console.log(fName.slice(2,6))
+// console.log(fName.substring(2,3))
+// console.log(fName.concat(" ",greet))
+// console.log(greet.replace("session", "Bootcamp"))
+
+let fName = "Pradesh"
+// console.log("My name is :", fName)
+// console.log(`My name is: ${fName}`)
+// console.log(`the sum of the numbers is ${4+5}`)
+// console.log("this is\nmy new peom")
+console.log(`this is
+    my new
+    poem that
+    I am not going 
+    to recite`)
+
