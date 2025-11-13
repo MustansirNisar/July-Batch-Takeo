50620d387e5193df0a2d241241ed2eb95512277e
Mustansir Nisar <140247214+MustansirNisar@users.noreply.github.com>
2025-11-13 09:04:49 +0530

    Add files via upload

---
+++ JavaScript/script.js
@@ -0,0 +1,61 @@
+// console.log("hey, this is really great")
+// var age;  //Variable Declaration
+// age = 25; //Initialization
+
+// const { use } = require("react");
+
+// console.log(age)
+// var age = 25; //Declaration + Initialization
+// console.log(age)
+
+// var age;
+// age = 23;
+
+// // var age = 25;
+// age = 25;
+// console.log(age)
+
+// let age = 30;
+// // let age = 40;
+// // let age; //Not allowed
+// age = 40;
+// console.log(age)
+
+
+// const age = 30;
+// // const age;
+// age = 40;
+// console.log(age)
+
+
+// let Age = 20;
+// let age = 30;
+// console.log(age)
+// console.log(Age)
+
+// let price = 222.33;
+// let age = 23;
+// console.log(typeof(price))
+// console.log(typeof(age))
+// let firstName = "Pradesh"
+// let something = 'hello world'
+// console.log(typeof(firstName))
+// console.log(typeof(something))
+
+// let id = "22";
+// console.log(typeof(id))
+
+// let isLoggedIn = true;
+// let hasAccess = false;
+// console.log(typeof(isLoggedIn))
+// console.log(typeof(hasAccess))
+
+// let price;
+// console.log(price)
+
+// let userName = null;
+// console.log(typeof(user))
+// console.log(user)
+
+let num = 23444;
+console.log(num)
